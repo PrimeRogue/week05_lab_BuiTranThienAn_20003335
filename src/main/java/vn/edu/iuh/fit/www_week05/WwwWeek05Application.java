e47ca2bcb1056f94146beea4468cde35e0c02d19
Bui Tran Thien An <buitranthienan1111@gmail.com>
2023-11-30 21:48:56 +0700

    Viet entities, repositories interface

---
+++ src/main/java/vn/edu/iuh/fit/www_week05/WwwWeek05Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_week05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwWeek05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WwwWeek05Application.class, args);
+    }
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/www_week05/WwwWeek05ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_week05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwWeek05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
